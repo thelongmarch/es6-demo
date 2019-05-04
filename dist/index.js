5ff3a2c6ccbcdbcba6de163e7d98857d51df50bc
thelongmarch <themarch@163.com>
2019-05-04 21:31:25 +0800

    es6

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// let a = 1;
+// console.log(a);
+
+var a = 0,
+    b = 1,
+    c = 2;
+
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// let a = 1;
+// console.log(a);
+
+let [a,b,c] = [0,1,2];
+console.log(a);
+console.log(b);
+console.log(c);
